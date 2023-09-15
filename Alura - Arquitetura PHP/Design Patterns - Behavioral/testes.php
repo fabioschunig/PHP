953435132bde6d8a161e80af87a06dedf09411c0
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-14 22:01:57 -0300

    Tests for CalculadoraDeDescontos class

--- Alura - Arquitetura PHP/Design Patterns - Behavioral/testes.php
+++ Alura - Arquitetura PHP/Design Patterns - Behavioral/testes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alura\DesignPattern\CalculadoraDeDescontos;
 use Alura\DesignPattern\CalculadoraDeImpostos;
 use Alura\DesignPattern\Impostos\ICMS;
 use Alura\DesignPattern\Impostos\ISS;
@@ -7,11 +8,27 @@ use Alura\DesignPattern\Orcamento;
 
 require 'vendor/autoload.php';
 
-$calculadora  = new CalculadoraDeImpostos();
-
 $orcamento = new Orcamento();
 $orcamento->valor = 100;
 
-echo $calculadora->calcula($orcamento, new ICMS()) . "\n";
+// Testes para CalculadoraDeImpostos
+$calculadoraImpostos  = new CalculadoraDeImpostos();
 
-echo $calculadora->calcula($orcamento, new ISS()) . "\n";
+echo "CalculadoraDeImpostos - ICMS: \n";
+echo $calculadoraImpostos->calcula($orcamento, new ICMS()) . "\n";
+
+echo "CalculadoraDeImpostos - ISS: \n";
+echo $calculadoraImpostos->calcula($orcamento, new ISS()) . "\n";
+
+// Testes para CalculadoraDeDescontos
+$calculadoraDescontos  = new CalculadoraDeDescontos();
+
+$orcamento->valor = 200;
+
+$orcamento->quantidadeItens = 5;
+echo "CalculadoraDeDescontos - 5 itens: \n";
+echo $calculadoraDescontos->calculaDescontos($orcamento) . "\n";
+
+$orcamento->quantidadeItens = 7;
+echo "CalculadoraDeDescontos - 7 itens: \n";
+echo $calculadoraDescontos->calculaDescontos($orcamento) . "\n";
