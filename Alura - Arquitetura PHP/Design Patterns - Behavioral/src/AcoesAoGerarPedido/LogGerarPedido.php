a457242d8b99b1d98be7158c6d23ad7fbfb71bfc
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-19 20:18:56 -0300

    LogGerarPedido class

---
+++ Alura - Arquitetura PHP/Design Patterns - Behavioral/src/AcoesAoGerarPedido/LogGerarPedido.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\DesignPattern\AcoesAoGerarPedido;
+
+class LogGerarPedido
+{
+    public function executaAcao(): void
+    {
+        echo "Gerando log de geração de pedido" . PHP_EOL;
+    }
+}
