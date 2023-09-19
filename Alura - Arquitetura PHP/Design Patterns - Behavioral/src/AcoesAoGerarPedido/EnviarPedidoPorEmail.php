82202af9319c8b03ae26146f5a4cc9a6bc4271a4
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-19 20:17:48 -0300

    EnviarPedidoPorEmail

---
+++ Alura - Arquitetura PHP/Design Patterns - Behavioral/src/AcoesAoGerarPedido/EnviarPedidoPorEmail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\DesignPattern\AcoesAoGerarPedido;
+
+class EnviarPedidoPorEmail
+{
+    public function executaAcao(): void
+    {
+        echo "Enviando e-mail de pedido gerado" . PHP_EOL;
+    }
+}
