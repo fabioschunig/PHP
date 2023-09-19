92ed2fd596dbe5ca771e8d5494ce3cbea81bbc10
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-19 20:16:03 -0300

    CriarPedidoNoBanco class

---
+++ Alura - Arquitetura PHP/Design Patterns - Behavioral/src/AcoesAoGerarPedido/CriarPedidoNoBanco.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\DesignPattern\AcoesAoGerarPedido;
+
+class CriarPedidoNoBanco
+{
+    public function executaAcao(): void
+    {
+        echo "Salvando pedido no banco de dados" . PHP_EOL;
+    }
+}
