8b66d39c53f90e866c0ee136be42eec4dfc01b21
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-21 22:29:54 -0300

    Better example for CurlHttpAdapter

--- Alura - Arquitetura PHP/Design Patterns - Structural/registro-orcamento.php
+++ Alura - Arquitetura PHP/Design Patterns - Structural/registro-orcamento.php
@@ -6,5 +6,11 @@ use Alura\DesignPattern\RegistroOrcamento;
 
 require 'vendor/autoload.php';
 
+$orcamento = new Orcamento();
+$orcamento->valor = 123.45;
+$orcamento->quantidadeItens = 7;
+$orcamento->aprova();
+$orcamento->finaliza();
+
 $registroOrcamento = new RegistroOrcamento(new CurlHttpAdapter());
-$registroOrcamento->registrar(new Orcamento);
+$registroOrcamento->registrar($orcamento);
--- Alura - Arquitetura PHP/Design Patterns - Structural/src/Http/CurlHttpAdapter.php
+++ Alura - Arquitetura PHP/Design Patterns - Structural/src/Http/CurlHttpAdapter.php
@@ -6,6 +6,8 @@ class CurlHttpAdapter implements HttpAdapter
 {
     public function post(string $url, array $data = []): void
     {
+        echo "curl http adapter" . PHP_EOL;
+
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_POST, $data);
         curl_exec($curl);
