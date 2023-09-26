4b600d709bd6c274ac2016064533200ad4c4c467
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-26 20:39:50 -0300

    RAM peak otimization

--- Alura - Arquitetura PHP/Design Patterns - Structural/pedidos.php
+++ Alura - Arquitetura PHP/Design Patterns - Structural/pedidos.php
@@ -6,11 +6,13 @@ use Alura\DesignPattern\Pedido;
 require 'vendor/autoload.php';
 
 $pedidos = [];
+// pequena otimização economiza 3MB de RAM
+$hoje = new \DateTimeImmutable();
 for ($i = 0; $i < 10000; $i++) {
     $pedido = new Pedido();
     $pedido->nomeCliente = md5((string) rand(1, 99999));
     $pedido->orcamento = new Orcamento;
-    $pedido->dataFinalizacao = new \DateTimeImmutable();
+    $pedido->dataFinalizacao = $hoje;
 
     $pedidos[] = $pedido;
 }
