d8804e34396412c14924295991143a6880a8fd85
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-26 20:37:38 -0300

    Test file for several Pedido objects

---
+++ Alura - Arquitetura PHP/Design Patterns - Structural/pedidos.php
@@ -0,0 +1,19 @@
+<?php
+
+use Alura\DesignPattern\Orcamento;
+use Alura\DesignPattern\Pedido;
+
+require 'vendor/autoload.php';
+
+$pedidos = [];
+for ($i = 0; $i < 10000; $i++) {
+    $pedido = new Pedido();
+    $pedido->nomeCliente = md5((string) rand(1, 99999));
+    $pedido->orcamento = new Orcamento;
+    $pedido->dataFinalizacao = new \DateTimeImmutable();
+
+    $pedidos[] = $pedido;
+}
+
+$memory = memory_get_peak_usage() / 1024 / 1024;
+echo $memory . ' MB' . PHP_EOL;
