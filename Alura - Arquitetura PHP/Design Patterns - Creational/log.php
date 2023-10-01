2f23d474ac60696a92c3aae0dfe0f80d7a60d508
Fabio Schunig <fabio.schunig@gmail.com>
2023-09-30 22:24:28 -0300

    FileLogManager test

--- Alura - Arquitetura PHP/Design Patterns - Creational/log.php
+++ Alura - Arquitetura PHP/Design Patterns - Creational/log.php
@@ -1,8 +1,10 @@
 <?php
 
+use Alura\DesignPattern\Log\FileLogManager;
 use Alura\DesignPattern\Log\StdoutLogManager;
 
 require 'vendor/autoload.php';
 
-$logManager = new StdoutLogManager();
+//$logManager = new StdoutLogManager();
+$logManager = new FileLogManager(__DIR__ . '/log.txt');
 $logManager->log('info', 'Teste de log - Stdout');
