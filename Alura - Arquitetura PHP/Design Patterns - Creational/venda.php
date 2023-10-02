faf8605109b25814c68748132abda170e63f76a4
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-01 22:38:56 -0300

    Comments on VendaServicoFactory test dumps

--- Alura - Arquitetura PHP/Design Patterns - Creational/venda.php
+++ Alura - Arquitetura PHP/Design Patterns - Creational/venda.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alura\DesignPattern\Venda\VendaProdutoFactory;
 use Alura\DesignPattern\Venda\VendaServicoFactory;
 
 require 'vendor/autoload.php';
@@ -9,4 +10,9 @@ $venda = $vendaFactory->criarVenda();
 $imposto = $vendaFactory->imposto();
 
 var_dump($venda);
+// dumps:
+// object(Alura\DesignPattern\Venda\VendaServico)
+
 var_dump($imposto);
+// dumps:
+// object(Alura\DesignPattern\Impostos\ISS)
