8da3bd7ddff616b17583e09b4c6e43ce8cee892c
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-03 23:04:21 -0300

    Produto and Servico builder tests

--- Alura - Arquitetura PHP/Design Patterns - Creational/nota-fiscal.php
+++ Alura - Arquitetura PHP/Design Patterns - Creational/nota-fiscal.php
@@ -2,16 +2,31 @@
 
 use Alura\DesignPattern\ItemOrcamento;
 use Alura\DesignPattern\NotaFiscal\ConstrutorNotaFiscal;
+use Alura\DesignPattern\NotaFiscal\ConstrutorNotaFiscalProduto;
+use Alura\DesignPattern\NotaFiscal\ConstrutorNotaFiscalServico;
 
 require 'vendor/autoload.php';
 
-$builder = new ConstrutorNotaFiscal();
+$builder = new ConstrutorNotaFiscalProduto();
+// $builder = new ConstrutorNotaFiscalServico();
+
+$item1 = new ItemOrcamento();
+$item1->valor = 500;
+
+$item2 = new ItemOrcamento();
+$item2->valor = 350;
+
+$item3 = new ItemOrcamento();
+$item3->valor = 700;
 
 $notaFiscal = $builder->paraEmpresa('12.3456.789/0001-00', 'Empresa de testes')
-    ->comItem(new ItemOrcamento())
-    ->comItem(new ItemOrcamento())
-    ->comItem(new ItemOrcamento())
+    ->comItem($item1)
+    ->comItem($item2)
+    ->comItem($item3)
     ->comObservacoes('Nota fiscal gerado com um construtor')
     ->constroi();
 
 print_r($notaFiscal);
+
+echo "Valor impostos:" . PHP_EOL;
+echo $notaFiscal->valorImpostos . PHP_EOL;
