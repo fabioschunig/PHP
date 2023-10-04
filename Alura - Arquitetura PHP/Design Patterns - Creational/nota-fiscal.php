8e1f96ad7cc03665e747d80a02ab24c8a5811dc3
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-03 22:28:23 -0300

    ConstrutorNotaFiscal test file

---
+++ Alura - Arquitetura PHP/Design Patterns - Creational/nota-fiscal.php
@@ -0,0 +1,15 @@
+<?php
+
+use Alura\DesignPattern\ItemOrcamento;
+use Alura\DesignPattern\NotaFiscal\ConstrutorNotaFiscal;
+
+require 'vendor/autoload.php';
+
+$builder = new ConstrutorNotaFiscal();
+$builder->paraEmpresa('12.3456.789/0001-00', 'Empresa de testes');
+$builder->comItem(new ItemOrcamento());
+$builder->comItem(new ItemOrcamento());
+$builder->comItem(new ItemOrcamento());
+$builder->comObservacoes('Nota fiscal gerado com um construtor');
+
+print_r($builder);
