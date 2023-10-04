93f10b1f4c7f9b57020353a61b814458d30e7325
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-04 20:39:04 -0300

    NotaFiscal clonar tests

---
+++ Alura - Arquitetura PHP/Design Patterns - Creational/clone.php
@@ -0,0 +1,37 @@
+<?php
+
+use Alura\DesignPattern\ItemOrcamento;
+use Alura\DesignPattern\NotaFiscal\ConstrutorNotaFiscalServico;
+
+require 'vendor/autoload.php';
+
+$builder = new ConstrutorNotaFiscalServico();
+
+$item1 = new ItemOrcamento();
+$item1->valor = 500;
+
+$item2 = new ItemOrcamento();
+$item2->valor = 350;
+
+$item3 = new ItemOrcamento();
+$item3->valor = 700;
+
+$notaFiscal = $builder->paraEmpresa('12.3456.789/0001-00', 'Empresa de testes')
+    ->comItem($item1)
+    ->comItem($item2)
+    ->comItem($item3)
+    ->comObservacoes('Nota fiscal gerado com um construtor')
+    ->constroi();
+
+echo "Nota fiscal:" . PHP_EOL;
+print_r($notaFiscal);
+
+$notaFiscal2 = $notaFiscal->clonar();
+
+$item4 = new ItemOrcamento();
+$item4->valor = 135;
+
+$notaFiscal2->itens[] = $item4;
+
+echo "Nota fiscal clone:" . PHP_EOL;
+print_r($notaFiscal2);
