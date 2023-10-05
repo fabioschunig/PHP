b92e95ac4510f92a07070ec35ce02b317a51f27e
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-04 21:00:26 -0300

    PDO test file

---
+++ Alura - Arquitetura PHP/Design Patterns - Creational/pdo.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$pdo = new \PDO('sqlite::memory:');
+echo "PDO:" . PHP_EOL;
+var_dump($pdo);
+
+$pdo2 = new \PDO('sqlite::memory:');
+echo "PDO2:" . PHP_EOL;
+var_dump($pdo2);
