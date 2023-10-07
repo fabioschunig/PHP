d4f69c745df6f9bf42ccdf0e8d361678a51e4da4
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-07 18:52:50 -0300

    E-mail validation with filter_var function

--- Alura - Arquitetura PHP/Clean Architecture/src/Email.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Email.php
@@ -8,6 +8,12 @@ class Email implements \Stringable
 
     public function __construct(string $endereco)
     {
+        if (filter_var($endereco, FILTER_VALIDATE_EMAIL) === false) {
+            throw new \InvalidArgumentException(
+                'Endereço de e-mail inválido'
+            );
+        }
+
         $this->endereco = $endereco;
     }
 
