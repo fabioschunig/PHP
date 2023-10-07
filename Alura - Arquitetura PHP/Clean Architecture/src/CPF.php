c7485251fec9113d5324aa5b1794c85fe81e4bc3
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-07 18:58:56 -0300

    Changed CPF class to implement Stringable

--- Alura - Arquitetura PHP/Clean Architecture/src/CPF.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/CPF.php
@@ -2,12 +2,17 @@
 
 namespace Alura\Arquitetura;
 
-class CPF
+class CPF implements \Stringable
 {
     private string $numero;
 
     public function __construct(string $numero)
     {
         $this->numero = $numero;
+    }
+
+    public function __toString(): string
+    {
+        return $this->numero;
     }
 }
