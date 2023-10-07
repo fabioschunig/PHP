ea715c82a6ff83e5feccf089cd9ff9917863e0bd
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-07 18:45:53 -0300

    Changed Email to implement Stringable

--- Alura - Arquitetura PHP/Clean Architecture/src/Email.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Email.php
@@ -2,12 +2,17 @@
 
 namespace Alura\Arquitetura;
 
-class Email
+class Email implements \Stringable
 {
     private string $endereco;
 
     public function __construct(string $endereco)
     {
         $this->endereco = $endereco;
+    }
+
+    public function __toString(): string
+    {
+        return $this->endereco;
     }
 }
