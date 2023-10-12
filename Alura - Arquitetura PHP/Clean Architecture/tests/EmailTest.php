08bd8c8cf36cf11c817160148c2314e2291aa521
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-11 21:24:43 -0300

    EmailTest class test file

---
+++ Alura - Arquitetura PHP/Clean Architecture/tests/EmailTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Alura\Arquitetura\Testes;
+
+use Alura\Arquitetura\Email;
+use PHPUnit\Framework\TestCase;
+
+class EmailTest extends TestCase
+{
+    public function testEmailComNumeroNoFormatoInvalidoNaoDevePoderExistir()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new Email('e-mail inválido');
+    }
+
+    public function testEmailDevePoderSerRepresentadoComoString()
+    {
+        $email = new Email('endereco@exemplo.com');
+        $this->assertSame('endereco@exemplo.com', (string) $email);
+    }
+}
