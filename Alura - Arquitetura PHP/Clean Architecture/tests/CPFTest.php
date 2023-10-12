1933dea24965955becf78bea56240710d4f7308b
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-11 21:20:14 -0300

    CPFTest class test file

---
+++ Alura - Arquitetura PHP/Clean Architecture/tests/CPFTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Alura\Arquitetura\Testes;
+
+use Alura\Arquitetura\CPF;
+use PHPUnit\Framework\TestCase;
+
+class CPFTest extends TestCase
+{
+    public function testCPFComNumeroNoFormatoInvalidoNaoDevePoderExistir()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new CPF('1234567890');
+    }
+
+    public function testCPFDevePoderSerRepresentadoComoString()
+    {
+        $cpf = new CPF('123.456.789-10');
+        $this->assertSame('123.456.789-10', (string) $cpf);
+    }
+}
