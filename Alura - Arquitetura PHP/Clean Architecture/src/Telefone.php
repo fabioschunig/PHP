edba0f3be92de7a1a6c98721a33bb2ff8d889186
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-11 21:32:18 -0300

    Telefone class

---
+++ Alura - Arquitetura PHP/Clean Architecture/src/Telefone.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+class Telefone
+{
+    private string $ddd;
+    private string $numero;
+
+    public function __construct(string $ddd, string $numero)
+    {
+        $this->ddd = $ddd;
+        $this->numero = $numero;
+    }
+}
