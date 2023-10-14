2bb5249920e082d06258c89f5a5692ad6f60427f
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-13 21:00:07 -0300

    Self-encapsulation and regexp to set DDD and phone number

--- Alura - Arquitetura PHP/Clean Architecture/src/Telefone.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Telefone.php
@@ -9,12 +9,34 @@ class Telefone implements \Stringable
 
     public function __construct(string $ddd, string $numero)
     {
-        $this->ddd = $ddd;
-        $this->numero = $numero;
+        $this->setDdd($ddd);
+        $this->setNumero($numero);
     }
 
     public function __toString(): string
     {
         return "(" . $this->ddd . ") " . $this->numero;
+    }
+
+    public function setDdd(string $ddd): void
+    {
+        if (preg_match('/\d{2}/', $ddd) !== 1) {
+            throw new \InvalidArgumentException(
+                'DDD inválido'
+            );
+        }
+
+        $this->ddd = $ddd;
+    }
+
+    public function setNumero(string $numero): void
+    {
+        if (preg_match('/\d{8,9}/', $numero) !== 1) {
+            throw new \InvalidArgumentException(
+                'Número de telefone inválido'
+            );
+        }
+
+        $this->numero = $numero;
     }
 }
