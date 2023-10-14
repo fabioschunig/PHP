178f185ea4381b845fccd652b702822ceeea81ef
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-13 21:51:38 -0300

    FabricaAluno class

---
+++ Alura - Arquitetura PHP/Clean Architecture/src/FabricaAluno.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Alura\Arquitetura;
+
+class FabricaAluno
+{
+    private Aluno $aluno;
+
+    public function comCPFEmailENome(string $cpf, string $email, string $nome): self
+    {
+        $this->aluno = new Aluno(
+            new CPF($cpf),
+            $nome,
+            new Email($email),
+        );
+
+        return $this;
+    }
+
+    public function adicionaTelefone(string $ddd, string $numero): self
+    {
+        $this->aluno->adicionarTelefone($ddd, $numero);
+
+        return $this;
+    }
+
+    public function aluno(): Aluno
+    {
+        return $this->aluno;
+    }
+}
