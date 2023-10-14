8d96bf56c7b08b19a573e637d47ded2a95156e2e
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-13 21:30:44 -0300

    Aluno class __construct with properties

--- Alura - Arquitetura PHP/Clean Architecture/src/Aluno.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Aluno.php
@@ -9,6 +9,13 @@ class Aluno
     private Email $email;
     private array $telefones;
 
+    public function __construct(CPF $cpf, string $nome, Email $email)
+    {
+        $this->cpf = $cpf;
+        $this->nome = $nome;
+        $this->email = $email;
+    }
+
     public function adicionarTelefone(string $ddd, string $numero)
     {
         $this->telefones[] = new Telefone($ddd, $numero);
