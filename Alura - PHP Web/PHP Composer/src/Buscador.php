4d0bf0c09fffdc9d812fc0a2376006f74b7f0d87
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-18 23:58:23 -0300

    Buscador contructor with httpClient and crawler

--- Alura - PHP Web/PHP Composer/src/Buscador.php
+++ Alura - PHP Web/PHP Composer/src/Buscador.php
@@ -2,6 +2,24 @@
 
 namespace Alura\BuscadorDeCursos;
 
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
 class Buscador
 {
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
 }
