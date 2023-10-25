443ec77871ea9145ba90cf36e3e61399998d2b71
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-24 22:23:54 -0300

    Create table students

--- Alura - PHP Web/PHP e PDO/conexao.php
+++ Alura - PHP Web/PHP e PDO/conexao.php
@@ -4,3 +4,7 @@ $arquivoBancoDados = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $arquivoBancoDados);
 
 echo "Conectado ao SQLite: " . $arquivoBancoDados . PHP_EOL;
+
+$sqlCreate = 'CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);';
+
+$pdo->exec($sqlCreate);
