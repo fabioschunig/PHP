3014d94991b3b7b6dc8704ab115fb1ce787456f0
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-26 20:26:37 -0300

    Insert with named parameters

--- Alura - PHP Web/PHP e PDO/inserir-aluno.php
+++ Alura - PHP Web/PHP e PDO/inserir-aluno.php
@@ -9,14 +9,14 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(
     null,
-    "Fabio ', ''); DROP TABLE students; -- Schunig",
-    new \DateTimeImmutable('1984-09-15')
+    "Teste nome estudante",
+    new \DateTimeImmutable('1998-03-07')
 );
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if ($statement->execute()) {
     echo "Aluno incluído" . PHP_EOL;
