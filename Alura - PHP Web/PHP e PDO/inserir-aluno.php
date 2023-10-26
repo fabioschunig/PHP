b6dda27d8947cbb611c4fb1591687a2eefd5f72e
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-26 20:20:34 -0300

    Insert student with prepare statement

--- Alura - PHP Web/PHP e PDO/inserir-aluno.php
+++ Alura - PHP Web/PHP e PDO/inserir-aluno.php
@@ -7,10 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Fabio Schunig', new \DateTimeImmutable('1984-09-15'));
+$student = new Student(
+    null,
+    "Fabio ', ''); DROP TABLE students; -- Schunig",
+    new \DateTimeImmutable('1984-09-15')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
 
-echo $sqlInsert . PHP_EOL;
-
-var_dump($pdo->exec($sqlInsert));
+if ($statement->execute()) {
+    echo "Aluno incluído" . PHP_EOL;
+}
