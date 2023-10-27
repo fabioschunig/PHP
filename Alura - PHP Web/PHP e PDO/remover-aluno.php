897d6d2d766ba0099124b0a98c71edc05d57c26f
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-26 22:25:19 -0300

    Test for remove method

--- Alura - PHP Web/PHP e PDO/remover-aluno.php
+++ Alura - PHP Web/PHP e PDO/remover-aluno.php
@@ -1,6 +1,22 @@
 <?php
 
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
 require_once 'vendor/autoload.php';
+
+$birthDate = new \DateTimeImmutable('1992-04-13');
+
+$repo = new PdoStudentRepository();
+$studentList = $repo->studentsBirthAt($birthDate);
+var_dump($studentList);
+
+if (count($studentList) > 0) {
+    $student = $studentList[0];
+    var_dump($repo->remove($student));
+}
+
+exit;
+
 
 $pdo = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
 
