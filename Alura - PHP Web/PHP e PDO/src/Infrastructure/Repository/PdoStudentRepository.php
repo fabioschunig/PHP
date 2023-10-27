a0687e7da14947ec61abd3b0caf2343c6342511a
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-26 21:31:48 -0300

    Database connection in PdoStudentRepository

--- Alura - PHP Web/PHP e PDO/src/Infrastructure/Repository/PdoStudentRepository.php
+++ Alura - PHP Web/PHP e PDO/src/Infrastructure/Repository/PdoStudentRepository.php
@@ -4,16 +4,23 @@ namespace Alura\Pdo\Infrastructure\Repository;
 
 use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Domain\Repository\StudentRepository;
-use DateTimeInterface;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
 class PdoStudentRepository implements StudentRepository
 {
+    private \PDO $connection;
+
+    public function __construct()
+    {
+        $this->connection = ConnectionCreator::createConnection();
+    }
+
     public function allStudents(): array
     {
         return array();
     }
 
-    public function studentsBirthAt(DateTimeInterface $birthDate): array
+    public function studentsBirthAt(\DateTimeInterface $birthDate): array
     {
         return array();
     }
