650f0ed4e81e1072ef3ce15816daa52a25d821e5
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-29 19:57:54 -0300

    Fixing file to create database and include phones table

--- Alura - PHP Web/PHP e PDO/conexao.php
+++ Alura - PHP Web/PHP e PDO/conexao.php
@@ -1,9 +1,25 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
 $pdo = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
 
-echo "Conectado ao SQLite: " . $arquivoBancoDados . PHP_EOL;
+echo "Conectado ao SQLite" . PHP_EOL;
 
-$sqlCreate = 'CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);';
+$sqlCreateDatabase = '
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_date TEXT
+    );
 
-$pdo->exec($sqlCreate);
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY (student_id) REFERENCES students (ID)
+    );
+';
+
+$pdo->exec($sqlCreateDatabase);
