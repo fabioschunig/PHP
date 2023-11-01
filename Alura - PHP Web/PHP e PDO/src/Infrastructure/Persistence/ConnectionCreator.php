e8974b4ad77003cedd5ca399028a7c2cea329541
Fabio Schunig <fabio.schunig@gmail.com>
2023-10-31 23:41:18 -0300

    Connection change test for MySQL

--- Alura - PHP Web/PHP e PDO/src/Infrastructure/Persistence/ConnectionCreator.php
+++ Alura - PHP Web/PHP e PDO/src/Infrastructure/Persistence/ConnectionCreator.php
@@ -8,9 +8,15 @@ class ConnectionCreator
 {
     public static function createConnection(): PDO
     {
-        $databasePath = __DIR__ . '/../../../banco.sqlite';
+        // $databasePath = __DIR__ . '/../../../banco.sqlite';
+        // $connection = new PDO('sqlite:' . $databasePath);
 
-        $connection = new PDO('sqlite:' . $databasePath);
+        $connection = new PDO(
+            'mysql:host=127.0.0.1;dbname=pdo',
+            'root',
+            '',
+        );
+
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
