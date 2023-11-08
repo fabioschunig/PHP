15cc292e0bd2afb437b34bf594298485b8f6ef61
Fabio Schunig <fabio.schunig@gmail.com>
2023-11-07 21:11:20 -0300

    Loop foreach to list items

--- Alura - PHP Web/PHP e MySQL/index.php
+++ Alura - PHP Web/PHP e MySQL/index.php
@@ -1,3 +1,61 @@
+<?php
+
+$produtosCafe = [
+    [
+        'nome' => "Café Cremoso",
+        'descricao' => "Café cremoso irresistivelmente suave e que envolve seu paladar",
+        'preco' => "5.00",
+        'imagem' => "img/cafe-cremoso.jpg"
+    ],
+    [
+        'nome' => "Café com Leite",
+        'descricao' => "A harmonia do café e do leite, uma experiência reconfortante",
+        'preco' => "2.00",
+        'imagem' => "img/cafe-com-leite.jpg"
+    ],
+    [
+        "nome" => "Cappuccino",
+        "descricao" => "Café suave, leite cremoso e uma pitada de sabor adocicado",
+        "preco" => "7.00",
+        "imagem" => "img/cappuccino.jpg"
+    ],
+    [
+        "nome" => "Café Gelado",
+        "descricao" => "Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.",
+        "preco" => "3.00",
+        "imagem" => "img/cafe-gelado.jpg"
+    ]
+];
+
+$produtosAlmoco = [
+    [
+        "nome" => "Bife",
+        "descricao" => "Bife, arroz com feijão e uma deliciosa batata frita",
+        "preco" => "27.90",
+        "imagem" => "img/bife.jpg"
+    ],
+    [
+        "nome" => "Filé de peixe",
+        "descricao" => "Filé de peixe salmão assado, arroz, feijão verde e tomate.",
+        "preco" => "24.99",
+        "imagem" => "img/prato-peixe.jpg"
+    ],
+    [
+        "nome" => "Frango",
+        "descricao" => "Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante",
+        "preco" => "23.00",
+        "imagem" => "img/prato-frango.jpg"
+    ],
+    [
+        "nome" => "Fettuccine",
+        "descricao" => "Prato italiano autêntico da massa do fettuccine com peito de frango grelhado",
+        "preco" => "22.50",
+        "imagem" => "img/fettuccine.jpg"
+    ]
+];
+
+?>
+
 <!doctype html>
 <html lang="pt-br">
 
@@ -29,38 +87,16 @@
                 <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-cafe-manha-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-cremoso.jpg">
+                <?php foreach ($produtosCafe as $cafe) : ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $cafe['imagem']; ?>">
+                        </div>
+                        <p><?= $cafe['nome']; ?></p>
+                        <p><?= $cafe['descricao']; ?></p>
+                        <p><?= "R$ " . $cafe['preco']; ?></p>
                     </div>
-                    <p>Café Cremoso</p>
-                    <p>Café cremoso irresistivelmente suave e que envolve seu paladar</p>
-                    <p>R$ 5.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-com-leite.jpg">
-                    </div>
-                    <p>Café com Leite</p>
-                    <p>A harmonia perfeita do café e do leite, uma experiência reconfortante</p>
-                    <p>R$ 2.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cappuccino.jpg">
-                    </div>
-                    <p>Cappuccino</p>
-                    <p>Café suave, leite cremoso e uma pitada de sabor adocicado</p>
-                    <p>R$ 7.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/cafe-gelado.jpg">
-                    </div>
-                    <p>Café Gelado</p>
-                    <p>Café gelado refrescante, adoçado e com notas sutis de baunilha ou caramelo.</p>
-                    <p>R$ 3.00</p>
-                </div>
+                <?php endforeach; ?>
             </div>
         </section>
         <section class="container-almoco">
@@ -69,38 +105,16 @@
                 <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
             </div>
             <div class="container-almoco-produtos">
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/bife.jpg">
+                <?php foreach ($produtosAlmoco as $almoco) : ?>
+                    <div class="container-produto">
+                        <div class="container-foto">
+                            <img src="<?= $almoco['imagem']; ?>">
+                        </div>
+                        <p><?= $almoco['nome']; ?></p>
+                        <p><?= $almoco['descricao']; ?></p>
+                        <p><?= "R$ " . $almoco['preco']; ?></p>
                     </div>
-                    <p>Bife</p>
-                    <p>Bife, arroz com feijão e uma deliciosa batata frita</p>
-                    <p>R$ 27.90</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-peixe.jpg">
-                    </div>
-                    <p>Filé de peixe</p>
-                    <p>Filé de peixe salmão assado, arroz, feijão verde e tomate.</p>
-                    <p>R$ 24.99</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/prato-frango.jpg">
-                    </div>
-                    <p>Frango</p>
-                    <p>Saboroso frango à milanesa com batatas fritas, salada de repolho e molho picante</p>
-                    <p>R$ 23.00</p>
-                </div>
-                <div class="container-produto">
-                    <div class="container-foto">
-                        <img src="img/fettuccine.jpg">
-                    </div>
-                    <p>Fettuccine</p>
-                    <p>Prato italiano autêntico da massa do fettuccine com peito de frango grelhado</p>
-                    <p>R$ 22.50</p>
-                </div>
+                <?php endforeach; ?>
             </div>
 
         </section>
