c6871e4b43df73b3d5f3097c028ba0d769d74f55
Fabio Schunig <fabio.schunig@gmail.com>
2023-11-20 21:53:23 -0300

    ProdutoRepositorio class

---
+++ Alura - PHP Web/PHP e MySQL/src/Repositorio/ProdutoRepositorio.php
@@ -0,0 +1,14 @@
+<?php
+
+class ProdutoRepositorio
+{
+    private PDO $pdo;
+
+    /**
+     * @param PDO $pdo
+     */
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+}
