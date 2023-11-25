b4acad2b8fc335529e89bb82142d8ab3927226c4
Fabio Schunig <fabio.schunig@gmail.com>
2023-11-24 21:19:09 -0300

    Query params for editar-produto

--- Alura - PHP Web/PHP e MySQL/admin.php
+++ Alura - PHP Web/PHP e MySQL/admin.php
@@ -54,7 +54,7 @@ $produtos = $produtosRepositorio->buscarTodos();
               <td><?= $produto->getTipo() ?></td>
               <td><?= $produto->getDescricao() ?></td>
               <td><?= $produto->getPrecoFormatado() ?></td>
-              <td><a class="botao-editar" href="editar-produto.html">Editar</a></td>
+              <td><a class="botao-editar" href="editar-produto.php?id=<?= $produto->getId() ?>">Editar</a></td>
               <td>
                 <form action="excluir-produto.php" method="post">
                   <input type="hidden" name="id" value="<?= $produto->getId() ?>">
