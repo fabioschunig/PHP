39441be65eaad306857d272e1366a0ff797c42ea
Fabio Schunig <fabio.schunig@gmail.com>
2023-11-24 22:14:16 -0300

    Uploading imagem files for produtos

--- Alura - PHP Web/PHP e MySQL/editar-produto.php
+++ Alura - PHP Web/PHP e MySQL/editar-produto.php
@@ -8,6 +8,12 @@ $produtosRepositorio = new ProdutoRepositorio($pdo);
 
 if (isset($_POST['editar'])) {
   $produto = new Produto($_POST['id'], $_POST['tipo'], $_POST['nome'], $_POST['descricao'], $_POST['preco']);
+
+  if ($_FILES['imagem']['error'] == UPLOAD_ERR_OK) {
+    $produto->setImagem(uniqid() . $_FILES['imagem']['name']);
+    move_uploaded_file($_FILES['imagem']['tmp_name'], $produto->getImagemDiretorio());
+  }
+
   $produtosRepositorio->atualizar($produto);
   header("Location: admin.php");
 } else {
@@ -43,7 +49,7 @@ if (isset($_POST['editar'])) {
       <img class="ornaments" src="img/ornaments-coffee.png" alt="ornaments">
     </section>
     <section class="container-form">
-      <form method="post">
+      <form method="post" enctype="multipart/form-data">
 
         <label for="nome">Nome</label>
         <input type="text" id="nome" name="nome" placeholder="Digite o nome do produto" value="<?= $produto->getNome() ?>" required>
@@ -63,7 +69,7 @@ if (isset($_POST['editar'])) {
         <input type="text" id="descricao" name="descricao" placeholder="Digite uma descrição" value="<?= $produto->getDescricao() ?>" required>
 
         <label for="preco">Preço</label>
-        <input type="text" id="preco" name="preco" placeholder="Digite uma descrição" value="<?= number_format($produto->getPreco(), 2)?>" required>
+        <input type="text" id="preco" name="preco" placeholder="Digite uma descrição" value="<?= number_format($produto->getPreco(), 2) ?>" required>
 
         <label for="imagem">Envie uma imagem do produto</label>
         <input type="file" name="imagem" accept="image/*" id="imagem" placeholder="Envie uma imagem">
