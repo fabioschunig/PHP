08cee3bc8867e36d8b8b79507b86a7edfc3efac5
Fabio Schunig <fabio.schunig@gmail.com>
2023-12-29 23:12:16 -0300

    User authentication and route POST login

--- Alura - PHP Web/PHP na Web/config/routes.php
+++ Alura - PHP Web/PHP na Web/config/routes.php
@@ -10,4 +10,5 @@ return [
     'POST|/editar-video' => \Alura\Mvc\Controller\EditVideoController::class,
     'GET|/remover-video' => \Alura\Mvc\Controller\DeleteVideoController::class,
     'GET|/login' => \Alura\Mvc\Controller\LoginFormController::class,
+    'POST|/login' => \Alura\Mvc\Controller\LoginController::class,
 ];
