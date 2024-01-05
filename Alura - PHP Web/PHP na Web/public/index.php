67ebc4aa4ca2d76d5ecb86f4ed95ec1f5675442c
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-05 15:12:24 -0300

    Save session data before regenerate ID

--- Alura - PHP Web/PHP na Web/public/index.php
+++ Alura - PHP Web/PHP na Web/public/index.php
@@ -25,11 +25,16 @@ $pathInfo = $_SERVER['PATH_INFO'] ?? '/';
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
 session_start();
-session_regenerate_id();
 $isLoginRoute = $pathInfo === '/login';
 if (!array_key_exists('logado', $_SESSION) && (!$isLoginRoute)) {
     header('Location: /login');
     return;
+}
+if (isset($_SESSION['logado'])) {
+    $originalInfo = $_SESSION['logado'];
+    unset($_SESSION['logado']);
+    session_regenerate_id();
+    $_SESSION['logado'] = $originalInfo;
 }
 
 $key = "$httpMethod|$pathInfo";
