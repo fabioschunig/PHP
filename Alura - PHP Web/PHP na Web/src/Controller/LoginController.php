e7ea746767a72e387391d93f776b696f3d6c588a
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-05 14:59:37 -0300

    Using password_verify to update the password

--- Alura - PHP Web/PHP na Web/src/Controller/LoginController.php
+++ Alura - PHP Web/PHP na Web/src/Controller/LoginController.php
@@ -25,7 +25,16 @@ class LoginController implements Controller
         $statement->execute();
 
         $userData = $statement->fetch(\PDO::FETCH_ASSOC);
-        $correctPassword = password_verify($password, $userData['password'] ?? '');
+        $passwordHashed = $userData['password'] ?? '';
+        $correctPassword = password_verify($password, $passwordHashed);
+
+        if (password_needs_rehash($passwordHashed, PASSWORD_DEFAULT)) {
+            $sqlUpdatePassword = "UPDATE users SET password = ? WHERE id = ?";
+            $statement = $this->pdo->prepare($sqlUpdatePassword);
+            $statement->bindValue(1, password_hash($password, PASSWORD_DEFAULT));
+            $statement->bindValue(2, $userData['id']);
+            $statement->execute();
+        }
 
         if ($correctPassword) {
             $_SESSION['logado'] = true;
