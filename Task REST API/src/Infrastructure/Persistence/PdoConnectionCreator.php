09e1e09869511012a80433eeb06557effde85b5f
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-06 16:06:17 -0300

    PHP - Task REST API - New class PdoConnectionCreator

---
+++ Task REST API/src/Infrastructure/Persistence/PdoConnectionCreator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TaskRestApi\Infrastructure\Persistence;
+
+use PDO;
+
+class PdoConnectionCreator
+{
+    public static function createConnection(): PDO
+    {
+        $connection = new PDO(
+            'mysql:host=127.0.0.1;dbname=taskrestapi',
+            'root',
+            '',
+        );
+
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $connection;
+    }
+}
