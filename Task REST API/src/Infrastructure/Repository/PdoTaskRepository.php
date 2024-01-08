f9c3c0b5aeb437568ad262af898a5cf40e8bf554
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-08 17:36:17 -0300

    PHP - PdoTaskRepository now requires a PDO connection

--- Task REST API/src/Infrastructure/Repository/PdoTaskRepository.php
+++ Task REST API/src/Infrastructure/Repository/PdoTaskRepository.php
@@ -3,9 +3,17 @@
 namespace TaskRestApi\Infrastructure\Repository;
 
 use TaskRestApi\Domain\Repository\TaskRepository;
+use PDO;
 
 class PdoTaskRepository implements TaskRepository
 {
+    private PDO $connection;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
     public function allTasks(): array
     {
         return array();
