a67ff4bd5b9a831a72088a2af083dd131a1b55f5
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-09 22:49:56 -0300

    Adding a comment with POST request test

--- Alura - PHP Web/PHP na Web/src/Controller/NewJsonVideoController.php
+++ Alura - PHP Web/PHP na Web/src/Controller/NewJsonVideoController.php
@@ -7,6 +7,12 @@ namespace Alura\Mvc\Controller;
 use Alura\Mvc\Entity\Video;
 use Alura\Mvc\Repository\VideoRepository;
 
+// to test
+// - request type: POST
+// - url: http://localhost:8080/videos
+// - body: {"url": "https://www.youtube.com/embed/qrQ8f5uCYP0?si=ftGV-1LneZuY7Wwj", "title": "City and Colour"}
+// - cookie header: PHPSESSID=copy_cookie_value; Path=/;
+
 class NewJsonVideoController implements Controller
 {
     public function __construct(private VideoRepository $videoRepository)
