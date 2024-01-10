abe92bc6a3f17719059629194d9ab657683a568b
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-09 21:59:35 -0300

    JsonVideoListController implements video list in JSON

--- Alura - PHP Web/PHP na Web/src/Controller/JsonVideoListController.php
+++ Alura - PHP Web/PHP na Web/src/Controller/JsonVideoListController.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
-class JsonVideoListController
+use Alura\Mvc\Repository\VideoRepository;
+
+class JsonVideoListController implements Controller
 {
+    public function __construct(private VideoRepository $videoRepository)
+    {
+    }
+
+    public function processaRequisicao(): void
+    {
+        $videoList = $this->videoRepository->all();
+        echo json_encode($videoList);
+    }
 }
