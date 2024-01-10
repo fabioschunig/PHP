7deec93f5c2f7e1a958d16813a7e1f0577c2a489
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-09 22:10:02 -0300

    NewJsonVideoController in routes config

--- Alura - PHP Web/PHP na Web/config/routes.php
+++ Alura - PHP Web/PHP na Web/config/routes.php
@@ -13,4 +13,5 @@ return [
     'POST|/login' => \Alura\Mvc\Controller\LoginController::class,
     'GET|/logout' => \Alura\Mvc\Controller\LogoutController::class,
     'GET|/videos-json' => \Alura\Mvc\Controller\JsonVideoListController::class,
+    'POST|/videos' => \Alura\Mvc\Controller\NewJsonVideoController::class,
 ];
