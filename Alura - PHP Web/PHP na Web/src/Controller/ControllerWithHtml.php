b94a2c8878e87d3d478f403df1621aefd118b05c
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-14 21:17:34 -0300

    Context param with extract variables to render template

--- Alura - PHP Web/PHP na Web/src/Controller/ControllerWithHtml.php
+++ Alura - PHP Web/PHP na Web/src/Controller/ControllerWithHtml.php
@@ -8,8 +8,9 @@ class ControllerWithHtml
 {
     private const TEMPLATE_PATH = __DIR__ . '/../../views/';
 
-    protected function renderTemplate(string $templateName): void
+    protected function renderTemplate(string $templateName, array $context = []): void
     {
+        extract($context);
         require_once self::TEMPLATE_PATH . $templateName . '.php';
     }
 }
