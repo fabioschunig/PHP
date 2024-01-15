017864cf8d8196bde74875992706b0fb10090164
Fabio Schunig <fabio.schunig@gmail.com>
2024-01-14 21:13:58 -0300

    Method renderTemplate to require the template file

--- Alura - PHP Web/PHP na Web/src/Controller/ControllerWithHtml.php
+++ Alura - PHP Web/PHP na Web/src/Controller/ControllerWithHtml.php
@@ -6,4 +6,10 @@ namespace Alura\Mvc\Controller;
 
 class ControllerWithHtml
 {
+    private const TEMPLATE_PATH = __DIR__ . '/../../views/';
+
+    protected function renderTemplate(string $templateName): void
+    {
+        require_once self::TEMPLATE_PATH . $templateName . '.php';
+    }
 }
