af0aa488f9689cac921fb098a01bb3656cf688d0
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-11 18:06:01 -0300

    Flash message for NewVideoController

--- Alura - PHP Web/PHP na Web/src/Controller/NewVideoController.php
+++ Alura - PHP Web/PHP na Web/src/Controller/NewVideoController.php
@@ -18,12 +18,14 @@ class NewVideoController implements Controller
     {
         $url = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL);
         if ($url === false) {
-            header('Location: /?sucesso=0');
+            $_SESSION['error_message'] = 'URL inválida';
+            header('Location: /novo-video');
             return;
         }
         $titulo = filter_input(INPUT_POST, 'titulo');
         if ($titulo === false) {
-            header('Location: /?sucesso=0');
+            $_SESSION['error_message'] = 'Título inválido';
+            header('Location: /novo-video');
             return;
         }
 
@@ -45,7 +47,8 @@ class NewVideoController implements Controller
 
         $success = $this->videoRepository->add($video);
         if ($success === false) {
-            header('Location: /?sucesso=0');
+            $_SESSION['error_message'] = 'Erro ao cadastrar o vídeo';
+            header('Location: /novo-video');
         } else {
             header('Location: /?sucesso=1');
         }
