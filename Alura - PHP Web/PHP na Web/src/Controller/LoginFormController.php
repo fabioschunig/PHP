b73e23d2c1eba06f9603f49ce534377e814d9e70
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-24 19:18:13 -0300

    LoginFormController using PSR interfaces and response

--- Alura - PHP Web/PHP na Web/src/Controller/LoginFormController.php
+++ Alura - PHP Web/PHP na Web/src/Controller/LoginFormController.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 class LoginFormController implements Controller
 {
     use \Alura\Mvc\Helper\HtmlRenderTrait;
 
-    public function processaRequisicao(): void
+    public function processaRequisicao(ServerRequestInterface $request): ResponseInterface
     {
         if ((array_key_exists('logado', $_SESSION)) && ($_SESSION['logado'] === true)) {
-            header('Location: /');
-            return;
+            return new Response(302, [
+                'Location' => '/',
+            ]);
         }
 
-        echo $this->renderTemplate('login-form');
+        $loginForm = $this->renderTemplate('login-form');
+
+        return new Response(body: $loginForm);
     }
 }
