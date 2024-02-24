bbb4e74dfff80be43cc39bf846c563a97cd03ce8
Fabio Schunig <fabio.schunig@gmail.com>
2024-02-24 19:21:00 -0300

    LogoutController using PSR interfaces and response

--- Alura - PHP Web/PHP na Web/src/Controller/LogoutController.php
+++ Alura - PHP Web/PHP na Web/src/Controller/LogoutController.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace Alura\Mvc\Controller;
 
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 class LogoutController implements Controller
 {
-    public function processaRequisicao(): void
+    public function processaRequisicao(ServerRequestInterface $request): ResponseInterface
     {
         // session_destroy();
         // alternativa para não destruir sessão
         $_SESSION['logado'] = false;
         unset($_SESSION['logado']);
 
-        header('Location: /login');
+        return new Response(302, [
+            'Location' => '/login',
+        ]);
     }
 }
