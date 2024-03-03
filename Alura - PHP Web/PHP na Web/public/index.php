4a83f323196ad9b6a371a01a7bc9af33a14aaf1f
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-02 22:35:55 -0300

    Using Psr17Factory in index file

--- Alura - PHP Web/PHP na Web/public/index.php
+++ Alura - PHP Web/PHP na Web/public/index.php
@@ -44,16 +44,16 @@ if (array_key_exists($key, $routes)) {
 
 // https://packagist.org/packages/nyholm/psr7-server
 // Instanciate ANY PSR-17 factory implementations. Here is nyholm/psr7 as an example
-// $psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
 
-// $creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-//     $psr17Factory, // ServerRequestFactory
-//     $psr17Factory, // UriFactory
-//     $psr17Factory, // UploadedFileFactory
-//     $psr17Factory  // StreamFactory
-// );
+$creator = new \Nyholm\Psr7Server\ServerRequestCreator(
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
+);
 
-// $serverRequest = $creator->fromGlobals();
+$request = $creator->fromGlobals();
 
 /** @var Controller $controller */
 $response = $controller->processaRequisicao(new ServerRequest($httpMethod, $_SERVER['REQUEST_URI']));
