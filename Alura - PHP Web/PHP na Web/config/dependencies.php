148fe1e163fedf06f6bc04a0c941c67e5b89b21d
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-05 21:20:21 -0300

    Using definition with DI\create

--- Alura - PHP Web/PHP na Web/config/dependencies.php
+++ Alura - PHP Web/PHP na Web/config/dependencies.php
@@ -1,12 +1,17 @@
 <?php
 
 $builder = new \DI\ContainerBuilder();
+// $builder->addDefinitions([
+//     PDO::class => function (): PDO {
+//         $dbPath = __DIR__ . '/../banco.sqlite';
+//         return new PDO("sqlite:$dbPath");
+//     }
+// ]);
+$dbPath = __DIR__ . '/../banco.sqlite';
 $builder->addDefinitions([
-    PDO::class => function (): PDO {
-        $dbPath = __DIR__ . '/../banco.sqlite';
-        return new PDO("sqlite:$dbPath");
-    }
+    PDO::class => \DI\create(PDO::class)->constructor("sqlite:$dbPath"),
 ]);
+
 
 /** @var \Psr\Container\ContainerInterface $container */
 $container = $builder->build();
