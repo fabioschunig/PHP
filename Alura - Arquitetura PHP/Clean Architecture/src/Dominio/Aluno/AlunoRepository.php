6d5253ac14168e2f78828b9a67aebecaf771baec
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-15 16:02:40 -0300

    AlunoRepository interface methods definition

--- Alura - Arquitetura PHP/Clean Architecture/src/Dominio/Aluno/AlunoRepository.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Dominio/Aluno/AlunoRepository.php
@@ -2,6 +2,12 @@
 
 namespace Alura\Arquitetura\Dominio\Aluno;
 
+use Alura\Arquitetura\Dominio\CPF;
+
 interface AlunoRepository
 {
+    public function adicionar(Aluno $aluno): void;
+    public function buscarPorCpf(CPF $cpf): Aluno;
+    /** @return Aluno[] */
+    public function buscarTodos(): array;
 }
