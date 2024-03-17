3f7afe502c76b0f3dfcb6152e105f879fd1a6b9c
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-17 19:14:32 -0300

    Testing Aluno inserting

--- Alura - Arquitetura PHP/Clean Architecture/test-aluno.php
+++ Alura - Arquitetura PHP/Clean Architecture/test-aluno.php
@@ -1,3 +1,18 @@
 <?php
 
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\CPF;
+use Alura\Arquitetura\Dominio\Email;
+use Alura\Arquitetura\Infra\Aluno\AlunoRepositoryPdo;
+
 require __DIR__ . '/vendor/autoload.php';
+
+$databasePath = __DIR__ . '/database/database.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$cpf = new CPF('490.621.190-93');
+$email = new Email('teste@email.com');
+$aluno = new Aluno($cpf, 'Test Aluno', $email);
+
+$alunoRepository = new AlunoRepositoryPdo($pdo);
+$alunoRepository->adicionar($aluno);
