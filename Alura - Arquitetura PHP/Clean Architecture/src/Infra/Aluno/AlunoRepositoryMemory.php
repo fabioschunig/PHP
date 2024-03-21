dabb9780a2418288e69625a24ce98ed711e03f62
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-21 19:51:42 -0300

    AlunoRepositoryMemory adicionar method

--- Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
@@ -2,12 +2,16 @@
 
 namespace Alura\Arquitetura\Infra\Aluno;
 
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
 use Alura\Arquitetura\Dominio\Aluno\AlunoRepository;
 
 class AlunoRepositoryMemory implements AlunoRepository
 {
+    private array $alunos = [];
+
     public function adicionar(Aluno $aluno): void
     {
+        $this->alunos[] = $aluno;
     }
 
     public function buscarPorCpf(CPF $cpf): Aluno
