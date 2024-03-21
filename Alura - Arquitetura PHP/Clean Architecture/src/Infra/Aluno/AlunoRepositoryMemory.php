f008bfdc5f33162e4c15fc9ce53d50237aedf65b
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-21 20:14:47 -0300

    AlunoRepositoryMemory buscarPorCpf method

--- Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
@@ -3,7 +3,9 @@
 namespace Alura\Arquitetura\Infra\Aluno;
 
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\AlunoNaoEncontrado;
 use Alura\Arquitetura\Dominio\Aluno\AlunoRepository;
+use Alura\Arquitetura\Dominio\CPF;
 
 class AlunoRepositoryMemory implements AlunoRepository
 {
@@ -16,6 +18,16 @@ class AlunoRepositoryMemory implements AlunoRepository
 
     public function buscarPorCpf(CPF $cpf): Aluno
     {
+        $alunosFiltrados = array_filter(
+            $this->alunos,
+            fn (Aluno $aluno) => ($aluno->cpf() === $cpf)
+        );
+
+        if (count($alunosFiltrados) === 0) {
+            throw new AlunoNaoEncontrado($cpf);
+        }
+
+        return $alunosFiltrados[0];
     }
 
     public function buscarTodos(): array
