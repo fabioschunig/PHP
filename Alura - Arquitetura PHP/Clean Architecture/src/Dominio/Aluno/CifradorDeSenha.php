d514f6528aaea1e497dc1d646bd81acb88f2c47b
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-21 20:35:36 -0300

    Methods cifrar and verificar in CifradorDeSenha

--- Alura - Arquitetura PHP/Clean Architecture/src/Dominio/Aluno/CifradorDeSenha.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Dominio/Aluno/CifradorDeSenha.php
@@ -4,4 +4,6 @@ namespace Alura\Arquitetura\Dominio\Aluno;
 
 interface CifradorDeSenha
 {
+    public function cifrar(string $senha): string;
+    public function verificar(string $senhaEmText, string $senhaCifrada): bool;
 }
