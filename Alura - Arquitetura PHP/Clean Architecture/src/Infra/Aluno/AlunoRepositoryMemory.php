52f7d4ae830800f25b1d1d2b64635a5934d07671
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-21 19:49:07 -0300

    AlunoRepositoryMemory methods

--- Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
@@ -2,6 +2,19 @@
 
 namespace Alura\Arquitetura\Infra\Aluno;
 
+use Alura\Arquitetura\Dominio\Aluno\AlunoRepository;
+
 class AlunoRepositoryMemory implements AlunoRepository
 {
+    public function adicionar(Aluno $aluno): void
+    {
+    }
+
+    public function buscarPorCpf(CPF $cpf): Aluno
+    {
+    }
+
+    public function buscarTodos(): array
+    {
+    }
 }
