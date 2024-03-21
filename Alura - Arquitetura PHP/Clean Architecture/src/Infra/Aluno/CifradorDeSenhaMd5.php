686d8522ee5ec4bc8f21781a57e875e54730428d
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-21 20:41:28 -0300

    Methods cifrar and verificar with MD5

--- Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/CifradorDeSenhaMd5.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/CifradorDeSenhaMd5.php
@@ -6,4 +6,13 @@ use Alura\Arquitetura\Dominio\Aluno\CifradorDeSenha;
 
 class CifradorDeSenhaMd5 implements CifradorDeSenha
 {
+    public function cifrar(string $senha): string
+    {
+        return md5($senha);
+    }
+
+    public function verificar(string $senhaEmText, string $senhaCifrada): bool
+    {
+        return md5($senhaEmText) === $senhaCifrada;
+    }
 }
