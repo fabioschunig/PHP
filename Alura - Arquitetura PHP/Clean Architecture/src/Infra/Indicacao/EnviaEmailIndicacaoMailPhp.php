35798a4d666a9ea9607001ffb99c095bee40fa29
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-24 20:22:43 -0300

    EnviaEmailIndicacaoMailPhp class > implements EnviaEmailIndicacao

---
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Indicacao/EnviaEmailIndicacaoMailPhp.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Alura\Arquitetura\Infra\Indicacao;
+
+use Alura\Arquitetura\Aplicacao\Indicacao\EnviaEmailIndicacao;
+
+class EnviaEmailIndicacaoMailPhp implements EnviaEmailIndicacao
+{
+}
