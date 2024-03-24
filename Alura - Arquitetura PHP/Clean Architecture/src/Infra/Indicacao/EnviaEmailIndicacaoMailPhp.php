01d8595df878373de5d3e3e685a91de0f9896b43
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-24 20:24:49 -0300

    Method enviarPara in EnviaEmailIndicacaoMailPhp

--- Alura - Arquitetura PHP/Clean Architecture/src/Infra/Indicacao/EnviaEmailIndicacaoMailPhp.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Indicacao/EnviaEmailIndicacaoMailPhp.php
@@ -3,7 +3,16 @@
 namespace Alura\Arquitetura\Infra\Indicacao;
 
 use Alura\Arquitetura\Aplicacao\Indicacao\EnviaEmailIndicacao;
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
 
 class EnviaEmailIndicacaoMailPhp implements EnviaEmailIndicacao
 {
+    public function enviarPara(Aluno $alunoIndicado): void
+    {
+        mail(
+            $alunoIndicado->email(),
+            "Indicação",
+            "Indicação de aluno",
+        );
+    }
 }
