dc434239980781ab2925f49b976460e85a589902
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-28 23:28:16 -0300

    Fix bug in buscarPorCpf method

--- Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Infra/Aluno/AlunoRepositoryMemory.php
@@ -20,7 +20,7 @@ class AlunoRepositoryMemory implements AlunoRepository
     {
         $alunosFiltrados = array_filter(
             $this->alunos,
-            fn (Aluno $aluno) => ($aluno->cpf() === $cpf)
+            fn (Aluno $aluno) => ($aluno->cpf() === (string) $cpf)
         );
 
         if (count($alunosFiltrados) === 0) {
