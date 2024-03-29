adae663a87a8d39cc4b376cb2c3f8cb4f293fc11
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-28 22:21:47 -0300

    Method executar in MatricularAluno class to add Aluno

--- Alura - Arquitetura PHP/Clean Architecture/src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
@@ -2,6 +2,21 @@
 
 namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
 
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\AlunoRepository;
+
 class MatricularAluno
 {
+    private AlunoRepository $repository;
+
+    public function __construct(AlunoRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function executar(MatricularAlunoDto $dados)
+    {
+        $aluno = Aluno::comCPFNomeEEmail($dados->cpfAluno, $dados->nomeAluno, $dados->emailAluno);
+        $this->repository->adicionar($aluno);
+    }
 }
