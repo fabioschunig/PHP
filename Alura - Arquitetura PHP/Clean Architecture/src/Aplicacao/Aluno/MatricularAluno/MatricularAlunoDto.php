5f1027a33b84a27d9cbee84589e6e2b6a98f3bc8
Fabio Schunig <fabio.schunig@gmail.com>
2024-03-28 22:17:17 -0300

    MatricularAlunoDto construct with params

--- Alura - Arquitetura PHP/Clean Architecture/src/Aplicacao/Aluno/MatricularAluno/MatricularAlunoDto.php
+++ Alura - Arquitetura PHP/Clean Architecture/src/Aplicacao/Aluno/MatricularAluno/MatricularAlunoDto.php
@@ -4,4 +4,14 @@ namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
 
 class MatricularAlunoDto
 {
+    public string $cpfAluno;
+    public string $nomeAluno;
+    public string $emailAluno;
+
+    public function __construct(string $cpfAluno, string $nomeAluno, string $emailAluno)
+    {
+        $this->cpfAluno = $cpfAluno;
+        $this->nomeAluno = $nomeAluno;
+        $this->emailAluno = $emailAluno;
+    }
 }
