651e8001c526036ed7ab639bc13e8f24c135e777
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-01 20:32:44 -0300

    Test testAdicionarTelefoneAcimaDoMaximo for Aluno

--- Alura - Arquitetura PHP/Domain Driven Design/tests/Dominio/Aluno/AlunoTest.php
+++ Alura - Arquitetura PHP/Domain Driven Design/tests/Dominio/Aluno/AlunoTest.php
@@ -3,6 +3,7 @@
 namespace Alura\Arquitetura\Testes\Dominio\Aluno;
 
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\MaximoTelefonesAtingido;
 use Alura\Arquitetura\Dominio\CPF;
 use Alura\Arquitetura\Dominio\Email;
 use PHPUnit\Framework\TestCase;
@@ -20,5 +21,21 @@ class AlunoTest extends TestCase
         $aluno->adicionarTelefone('11', '111111111');
         $aluno->adicionarTelefone('99', '999999999');
         $this->assertSame(2, count($aluno->telefones()));
+    }
+
+    public function testAdicionarTelefoneAcimaDoMaximo()
+    {
+        $this->expectException(MaximoTelefonesAtingido::class);
+        $this->expectExceptionMessage("Somente é permitido o máximo de 2 telefones por aluno");
+
+        $aluno = new Aluno(
+            new CPF('123.456.789-10'),
+            'Teste telefone',
+            new Email('teste@email.com'),
+        );
+
+        $aluno->adicionarTelefone('11', '111111111');
+        $aluno->adicionarTelefone('55', '555555555');
+        $aluno->adicionarTelefone('99', '999999999');
     }
 }
