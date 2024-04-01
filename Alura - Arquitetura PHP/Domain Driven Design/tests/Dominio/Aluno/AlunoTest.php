10b369d6ec2b4f66a98ea99375b02f4de91d7ada
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-01 20:10:58 -0300

    Test testAdicionarTelefoneAbaixoDoMaximo for Aluno

--- Alura - Arquitetura PHP/Domain Driven Design/tests/Dominio/Aluno/AlunoTest.php
+++ Alura - Arquitetura PHP/Domain Driven Design/tests/Dominio/Aluno/AlunoTest.php
@@ -2,8 +2,23 @@
 
 namespace Alura\Arquitetura\Testes\Dominio\Aluno;
 
+use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\CPF;
+use Alura\Arquitetura\Dominio\Email;
 use PHPUnit\Framework\TestCase;
 
 class AlunoTest extends TestCase
 {
+    public function testAdicionarTelefoneAbaixoDoMaximo()
+    {
+        $aluno = new Aluno(
+            new CPF('123.456.789-10'),
+            'Teste telefone',
+            new Email('teste@email.com'),
+        );
+
+        $aluno->adicionarTelefone('11', '111111111');
+        $aluno->adicionarTelefone('99', '999999999');
+        $this->assertSame(2, count($aluno->telefones()));
+    }
 }
