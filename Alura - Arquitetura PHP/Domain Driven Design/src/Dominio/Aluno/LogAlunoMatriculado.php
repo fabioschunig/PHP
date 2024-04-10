6f65cc13034c9324bbd1759025de8497a5ff556c
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-09 22:14:16 -0300

    Method reageAo to log AlunoMatriculado event

--- Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/Aluno/LogAlunoMatriculado.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/Aluno/LogAlunoMatriculado.php
@@ -4,4 +4,13 @@ namespace Alura\Arquitetura\Dominio\Aluno;
 
 class LogAlunoMatriculado
 {
+    public function reageAo(AlunoMatriculado $alunoMatriculado): void
+    {
+        fprintf(
+            STDERR,
+            'Aluno com CPF %s foi matriculado em %s',
+            (string) $alunoMatriculado->cpfAluno(),
+            $alunoMatriculado->momento()->format('d/m/Y'),
+        );
+    }
 }
