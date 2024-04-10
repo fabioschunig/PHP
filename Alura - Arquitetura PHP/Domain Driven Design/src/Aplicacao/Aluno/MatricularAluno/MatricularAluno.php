5efd4e12086a9f8fdefeda868350be806a066f8a
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-09 22:43:07 -0300

    Added listener to MatricularAluno class

--- Alura - Arquitetura PHP/Domain Driven Design/src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Aplicacao/Aluno/MatricularAluno/MatricularAluno.php
@@ -3,20 +3,29 @@
 namespace Alura\Arquitetura\Aplicacao\Aluno\MatricularAluno;
 
 use Alura\Arquitetura\Dominio\Aluno\Aluno;
+use Alura\Arquitetura\Dominio\Aluno\AlunoMatriculado;
 use Alura\Arquitetura\Dominio\Aluno\AlunoRepository;
+use Alura\Arquitetura\Dominio\Aluno\LogAlunoMatriculado;
+use Alura\Arquitetura\Dominio\PublicadorDeEvento;
 
 class MatricularAluno
 {
     private AlunoRepository $repository;
+    private PublicadorDeEvento $publicador;
 
     public function __construct(AlunoRepository $repository)
     {
         $this->repository = $repository;
+        $this->publicador = new PublicadorDeEvento();
+        $this->publicador->adicionarOuvinte(new LogAlunoMatriculado());
     }
 
     public function executar(MatricularAlunoDto $dados)
     {
         $aluno = Aluno::comCPFNomeEEmail($dados->cpfAluno, $dados->nomeAluno, $dados->emailAluno);
         $this->repository->adicionar($aluno);
+
+        $evento = new AlunoMatriculado($aluno->cpf());
+        $this->publicador->publicar($evento);
     }
 }
