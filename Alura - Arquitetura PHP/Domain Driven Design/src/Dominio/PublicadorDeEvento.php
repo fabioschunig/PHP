8e327187286e50723ae739d08dc61b4489a8b9bc
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-09 22:31:37 -0300

    Methods to add listener in PublicadorDeEvento class

--- Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/PublicadorDeEvento.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/PublicadorDeEvento.php
@@ -4,4 +4,17 @@ namespace Alura\Arquitetura\Dominio;
 
 class PublicadorDeEvento
 {
+    private array $ouvintes = [];
+
+    public function adicionarOuvinte(OuvinteDeEvento $ouvinte): void
+    {
+        $this->ouvintes[] = $ouvinte;
+    }
+
+    public function publicar(Evento $evento)
+    {
+        foreach ($this->ouvintes as $ouvinte) {
+            $ouvinte->processa($evento);
+        }
+    }
 }
