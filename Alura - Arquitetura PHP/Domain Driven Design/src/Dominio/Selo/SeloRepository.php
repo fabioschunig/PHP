6c612d4937a8efa3943b33c8028ca8623ec3cdfe
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-11 21:08:13 -0300

    SeloRepositoryMemory implements SeloRepository

--- Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/Selo/SeloRepository.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/Selo/SeloRepository.php
@@ -6,6 +6,6 @@ use Alura\Arquitetura\Dominio\CPF;
 
 interface SeloRepository
 {
-    public function adiciona(Selo $selo);
-    public function selosDeAlunoComCpf(CPF $cpf);
+    public function adiciona(Selo $selo): void;
+    public function selosDeAlunoComCpf(CPF $cpf): array;
 }
--- Alura - Arquitetura PHP/Domain Driven Design/src/Infra/Selo/SeloRepositoryMemory.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Infra/Selo/SeloRepositoryMemory.php
@@ -2,6 +2,24 @@
 
 namespace Alura\Arquitetura\Infra\Selo;
 
-class SeloRepositoryMemory
+use Alura\Arquitetura\Dominio\CPF;
+use Alura\Arquitetura\Dominio\Selo\Selo;
+use Alura\Arquitetura\Dominio\Selo\SeloRepository;
+
+class SeloRepositoryMemory implements SeloRepository
 {
+    private array $selos = [];
+
+    public function adiciona(Selo $selo): void
+    {
+        $this->selos[] = $selo;
+    }
+
+    public function selosDeAlunoComCpf(CPF $cpf): array
+    {
+        return array_filter(
+            $this->selos,
+            fn (Selo $selo) => $selo->cpfAluno() == $cpf,
+        );
+    }
 }
