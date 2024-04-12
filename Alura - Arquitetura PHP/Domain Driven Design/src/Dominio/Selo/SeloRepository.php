80b21df17f13d410844d5497b90c5628f66021c7
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-11 21:03:00 -0300

    SeloRepository methods

--- Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/Selo/SeloRepository.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Dominio/Selo/SeloRepository.php
@@ -2,6 +2,10 @@
 
 namespace Alura\Arquitetura\Dominio\Selo;
 
+use Alura\Arquitetura\Dominio\CPF;
+
 interface SeloRepository
 {
+    public function adiciona(Selo $selo);
+    public function selosDeAlunoComCpf(CPF $cpf);
 }
