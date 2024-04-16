d476764dc1c596072ad4e76106e626227a6cd547
Fabio Schunig <fabio.schunig@gmail.com>
2024-04-15 23:02:35 -0300

    Fixing CPF namespace in Gamificacao context

--- Alura - Arquitetura PHP/Domain Driven Design/src/Gamificacao/Infra/Selo/SeloRepositoryMemory.php
+++ Alura - Arquitetura PHP/Domain Driven Design/src/Gamificacao/Infra/Selo/SeloRepositoryMemory.php
@@ -2,7 +2,7 @@
 
 namespace Alura\Arquitetura\Gamificacao\Infra\Selo;
 
-use Alura\Arquitetura\Academico\Dominio\CPF;
+use Alura\Arquitetura\Shared\Dominio\CPF;
 use Alura\Arquitetura\Gamificacao\Dominio\Selo\Selo;
 use Alura\Arquitetura\Gamificacao\Dominio\Selo\SeloRepository;
 
