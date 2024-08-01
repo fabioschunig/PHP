69d7bd4c1ab214a140940302eadac09c32c06375
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-31 23:13:58 -0300

    Crypt user's password with MD5 hash

--- Login-Token/insert.php
+++ Login-Token/insert.php
@@ -8,9 +8,13 @@ $login = 'test@email.com';
 // insert with plain text password
 $password = '123456';
 
+// hash MD5 - not recommended anymore
+$hashMd5 = md5($password);
+
 $sql = 'INSERT INTO user (login, password) VALUES (?, ?)';
 $stm = $pdo->prepare($sql);
 $stm->bindValue(1, $login);
-$stm->bindValue(2, $password);
+// $stm->bindValue(2, $password);
+$stm->bindValue(2, $hashMd5);
 
 $stm->execute();
