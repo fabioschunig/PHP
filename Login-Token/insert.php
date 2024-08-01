e77509365365db25dc52d974e3dafdf479ef88ae
Fabio Schunig <fabio.schunig@gmail.com>
2024-07-31 23:20:53 -0300

    Hashing password with PHP default algorithm

--- Login-Token/insert.php
+++ Login-Token/insert.php
@@ -11,10 +11,14 @@ $password = '123456';
 // hash MD5 - not recommended anymore
 $hashMd5 = md5($password);
 
+// using default algorithm
+$hashDefault = password_hash($password, PASSWORD_DEFAULT);
+
 $sql = 'INSERT INTO user (login, password) VALUES (?, ?)';
 $stm = $pdo->prepare($sql);
 $stm->bindValue(1, $login);
 // $stm->bindValue(2, $password);
-$stm->bindValue(2, $hashMd5);
+// $stm->bindValue(2, $hashMd5);
+$stm->bindValue(2, $hashDefault);
 
 $stm->execute();
