5aec024e68b90bc07e84f5da5dfcceecd254ecf9
Fabio Schunig <fabio.schunig@gmail.com>
2024-08-06 20:35:12 -0300

    Saving token in sessionStorage

--- Login-Token/testing-javascript.js
+++ Login-Token/testing-javascript.js
@@ -5,3 +5,8 @@
 fetch('http://localhost:8080/login.php')
     .then(res => res.text())
     .then(token => console.log(token));
+
+// using session storage
+fetch('http://localhost:8080/login.php')
+    .then(res => res.text())
+    .then(token => sessionStorage.setItem('token', token));
