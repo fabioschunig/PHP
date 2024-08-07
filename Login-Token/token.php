ab04a295d690638d6da0b1a6f944ff7ca3dfc411
Fabio Schunig <fabio.schunig@gmail.com>
2024-08-06 22:59:25 -0300

    Retriving,  decoding and using token

--- Login-Token/token.php
+++ Login-Token/token.php
@@ -1,3 +1,11 @@
 <?php
 
-print_r($_SERVER);
+//print_r($_SERVER);
+
+$token = $_SERVER['HTTP_AUTHORIZATION'];
+
+$decoded = base64_decode($token);
+
+$user = str_replace('token:', '', $decoded);
+
+echo $user . PHP_EOL;
