94e461c6ebb8549eac6230eab9c2ad916fc010da
Fabio Schunig <fabio.schunig@gmail.com>
2024-08-07 23:30:50 -0300

    Creating JWT token from login e-mail

--- Login-Token/jwt.php
+++ Login-Token/jwt.php
@@ -1,6 +1,10 @@
 <?php
 
-print_r($_SERVER);
+require 'vendor/autoload.php';
+
+use Firebase\JWT\JWT;
+
+// print_r($_SERVER);
 
 $email = 'test@email.com';
 
@@ -8,4 +12,8 @@ if (isset($_GET['email'])) {
     $email = $_GET['email'];
 }
 
-echo $email;
+// echo $email;
+
+$tokenJwt = JWT::encode(['user' => $email], 'private_key', 'HS256');
+
+echo $tokenJwt;
