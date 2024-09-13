04da73cd586a3ef95daa6009909589f4d5306a96
Fabio Schunig <fabio.schunig@gmail.com>
2024-09-12 22:30:00 -0300

    Database script file

---
+++ Simple REST API/database/database.sql
@@ -0,0 +1,13 @@
+--------
+-- MySQL
+CREATE DATABASE simple_rest_api;
+
+use simple_rest_api;
+
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(128) COLLATE utf8mb4_unicode_ci NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
