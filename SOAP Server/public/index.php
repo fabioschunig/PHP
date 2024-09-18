abf89a54349288cf65f6b2a12950e913fecd0ea0
Fabio Schunig <fabio.schunig@gmail.com>
2024-09-18 20:39:26 -0300

    Using POST to submit

--- SOAP Server/public/index.php
+++ SOAP Server/public/index.php
@@ -2,7 +2,7 @@
 
 <p>Testing SOAP call with calculation of two numbers</p>
 
-<form action="client.php">
+<form action="client.php" method="post">
     <input type="text" name="number1" value="1">
     <br>
     <input type="text" name="number2" value="2">
