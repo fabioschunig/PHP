c7897d4d54e150eca46ef4b706fad729db8f5185
Fabio Schunig <fabio.schunig@gmail.com>
2024-09-18 20:33:34 -0300

    Index file with a form to submit the calculation do SOAP server

--- SOAP Server/public/index.php
+++ SOAP Server/public/index.php
@@ -1,3 +1,11 @@
-<?php
+<h1>SOAP Server test</h1>
 
-echo "SOAP Server";
+<p>Testing SOAP call with calculation of two numbers</p>
+
+<form action="client.php">
+    <input type="text" name="number1" value="1">
+    <br>
+    <input type="text" name="number2" value="2">
+    <br>
+    <button type="submit">Submit</button>
+</form>
