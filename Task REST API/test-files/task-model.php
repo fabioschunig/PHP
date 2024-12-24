717ad33cebe04f31e8e7822008a7080b6e0a8408
Fabio Schunig <fabio.schunig@gmail.com>
2024-12-24 20:33:19 -0300

    PHP - Task REST API - Testing PdoTaskRepository and allTasks method

--- Task REST API/test-files/task-model.php
+++ Task REST API/test-files/task-model.php
@@ -4,3 +4,9 @@ require_once 'vendor/autoload.php';
 
 $task = new \TaskRestApi\Domain\Model\Task;
 var_dump($task);
+
+$taskRepository = new \TaskRestApi\Infrastructure\Repository\PdoTaskRepository;
+var_dump($taskRepository);
+
+$tasks = $taskRepository->allTasks();
+var_dump($tasks);
