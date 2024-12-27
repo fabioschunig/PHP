7c51636433c29dbb87b2c30c017ea33692ba9f8e
Fabio Schunig <fabio.schunig@gmail.com>
2024-12-27 18:12:47 -0300

    PHP - Task REST API - Echo of Task for test

--- Task REST API/test-files/task-model.php
+++ Task REST API/test-files/task-model.php
@@ -5,7 +5,9 @@ require_once 'vendor/autoload.php';
 $task = new \TaskRestApi\Domain\Model\Task(1, 'Test Task 1', 'Tag 1', 0);
 var_dump($task);
 
-echo "\n" . $task->description() . "\n\n";
+echo $task->description() . "\n";
+echo $task->tags() . "\n";
+echo $task->status() . "\n";
 
 $taskRepository = new \TaskRestApi\Infrastructure\Repository\PdoTaskRepository;
 var_dump($taskRepository);
